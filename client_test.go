f9c3cc1a16aeb6ac8c63cb15ef001f9d8dead710
Alex Suraci <suraci.alex@gmail.com>
2015-10-04 15:34:01 -0700

    fix units which apparently exist

--- client_test.go
+++ client_test.go
@@ -19,16 +19,14 @@ import (
 var _ = Describe("Baggage Claim Client", func() {
 	Describe("getting the heartbeat interval from a TTL", func() {
 		It("has an upper bound of 1 minute", func() {
-			ttlInSeconds := uint(500)
-			interval := client.IntervalForTTL(ttlInSeconds)
+			interval := client.IntervalForTTL(500 * time.Second)
 
 			Expect(interval).To(Equal(time.Minute))
 		})
 
 		Context("when the TTL is small", func() {
 			It("Returns an interval that is half of the TTL", func() {
-				ttlInSeconds := uint(5)
-				interval := client.IntervalForTTL(ttlInSeconds)
+				interval := client.IntervalForTTL(5 * time.Second)
 
 				Expect(interval).To(Equal(2500 * time.Millisecond))
 			})
