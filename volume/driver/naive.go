35dc9cec668982f2ca6f47a4c707e76a2ba67e88
Jens Deppe <jdeppe@pivotal.io>
2017-04-18 10:29:40 -0700

    Improve/fix du and cp commands for non-Linux platforms
    - On Windows, robocopy is much more performant at copying things around
      and can also be used as a 'du' replacement. Now also there is less
      need for cygwin/Gnuwin frameworks.
    - The way du was being called was generally also broken.

--- volume/driver/naive.go
+++ volume/driver/naive.go
@@ -1,10 +1,7 @@
 package driver
 
 import (
-	"bytes"
-	"fmt"
 	"os"
-	"os/exec"
 )
 
 type NaiveDriver struct{}
@@ -17,25 +14,3 @@ func (driver *NaiveDriver) DestroyVolume(path string) error {
 	return os.RemoveAll(path)
 }
 
-func (driver *NaiveDriver) CreateCopyOnWriteLayer(path string, parent string) error {
-	return exec.Command("cp", "-r", parent, path).Run()
-}
-
-func (driver *NaiveDriver) GetVolumeSizeInBytes(path string) (int64, error) {
-	stdout := &bytes.Buffer{}
-	cmd := exec.Command("du", path)
-	cmd.Stdout = stdout
-
-	err := cmd.Run()
-	if err != nil {
-		return 0, err
-	}
-
-	var size int64
-	_, err = fmt.Sscanf(stdout.String(), "%d", &size)
-	if err != nil {
-		return 0, err
-	}
-
-	return size, nil
-}
