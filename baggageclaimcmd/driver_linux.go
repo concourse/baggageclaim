178b8c0d19c856ce5127764f259c4691edc0e96f
Alex Suraci <suraci.alex@gmail.com>
2017-05-25 11:11:55 -0400

    complete the move of driver detection down

--- baggageclaimcmd/driver_linux.go
+++ baggageclaimcmd/driver_linux.go
@@ -7,18 +7,24 @@ import (
 
 	"code.cloudfoundry.org/lager"
 	"github.com/concourse/baggageclaim/fs"
+	"github.com/concourse/baggageclaim/kernel"
 	"github.com/concourse/baggageclaim/volume"
 	"github.com/concourse/baggageclaim/volume/driver"
 )
 
+const btrfsFSType = 0x9123683e
+
 func (cmd *BaggageclaimCommand) driver(logger lager.Logger) (volume.Driver, error) {
 	var fsStat syscall.Statfs_t
-	err = syscall.Statfs(volumesDir, &fsStat)
+	err := syscall.Statfs(cmd.VolumesDir.Path(), &fsStat)
 	if err != nil {
 		return nil, fmt.Errorf("failed to stat volumes filesystem: %s", err)
 	}
 
-	kernelSupportsOverlay := kernel.CheckKernelVersion(4, 0, 0)
+	kernelSupportsOverlay, err := kernel.CheckKernelVersion(4, 0, 0)
+	if err != nil {
+		return nil, fmt.Errorf("failed to check kernel version: %s", err)
+	}
 
 	if cmd.Driver == "detect" {
 		if fsStat.Type == btrfsFSType {
@@ -52,14 +58,21 @@ func (cmd *BaggageclaimCommand) driver(logger lager.Logger) (volume.Driver, erro
 		return nil, errors.New("overlay driver requires kernel version >= 4.0.0")
 	}
 
+	logger.Info("using-driver", lager.Data{"driver": cmd.Driver})
+
+	var d volume.Driver
 	switch cmd.Driver {
 	case "overlay":
-		return &driver.OverlayDriver{
+		d = &driver.OverlayDriver{
 			OverlaysDir: cmd.OverlaysDir,
 		}
 	case "btrfs":
-		return driver.NewBtrFSDriver(logger.Session("driver"), cmd.BtrfsBin)
+		d = driver.NewBtrFSDriver(logger.Session("driver"), cmd.BtrfsBin)
+	case "naive":
+		d = &driver.NaiveDriver{}
+	default:
+		return nil, fmt.Errorf("unknown driver: %s", cmd.Driver)
 	}
 
-	return &driver.NaiveDriver{}, nil
+	return d, nil
 }
